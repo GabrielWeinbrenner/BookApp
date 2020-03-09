e9b640da77ce347e25b6f913488bf06401e68201
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-03-09 18:43:52 -0400

    Iniital

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color:"red"}}>Hello How are you</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
